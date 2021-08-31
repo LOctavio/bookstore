7e5d77dafc5a29c8d30be9e1f7d53caa5a290c0f
LOctavio <luis_ram1997@hotmail.com>
2021-08-31 12:46:09 -0500

    Add book's actions and reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,4 +1,27 @@
-import CreateNewBook from '../../components/CreateNewBook';
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const books = () => (<CreateNewBook />);
-export default books;
+const initialState = [];
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
