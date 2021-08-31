f7acde6649da405620a1ffad5a111451d0b5423b
LOctavio <luis_ram1997@hotmail.com>
2021-08-31 13:20:26 -0500

    Add state and function for adding books

--- src/components/CreateNewBook.js
+++ src/components/CreateNewBook.js
@@ -1,9 +1,38 @@
-const CreateNewBook = () => (
-  <form>
-    <input type="text" className="title" placeholder="title" />
-    <input type="text" className="author" placeholder="author" />
-    <button type="submit">submit</button>
-  </form>
-);
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
+
+const CreateNewBook = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const submitBookToStore = () => {
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <form>
+      <input type="text" className="title" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input type="text" className="author" placeholder="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+      <button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          submitBookToStore();
+        }}
+      >
+        submit
+      </button>
+    </form>
+  );
+};
 
 export default CreateNewBook;
