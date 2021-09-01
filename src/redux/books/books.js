da151a7d7a78412f0f1f04862d91045fc64bcf2c
LOctavio <luis_ram1997@hotmail.com>
2021-09-01 13:41:11 -0500

    Post item in API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,6 +3,21 @@ const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
+const addBookToAPI = async (book) => {
+  await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/eTfxKsR9wf3ubgm9W4xT/books', {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: book.id,
+      title: book.title,
+      category: book.category,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(() => console.log('added'));
+};
+
 export const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
@@ -16,6 +31,7 @@ export const removeBook = (payload) => ({
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
+      addBookToAPI(action.payload);
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload.id);
